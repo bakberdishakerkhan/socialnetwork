3ded35ac242eb27436011990d2b55aee512dc392
bakberdishakerkhan <88204773+bakberdishakerkhan@users.noreply.github.com>
2025-04-18 13:52:45 +0600

    4 lab

---
+++ instagramcopy/newslent/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.newslist, name='news_list'), 
+    path('<int:pid>/', views.postdetails, name='details'),
+    path('add/', views.addPost, name='add-new-post'),
+    path('<int:pid>/edit/', views.editPost, name='edit-post' ),
+    path('<int:pid>/delete/', views.deletePost, name='delete-post' ),
+
+]
--- instagramcopy/newslent/views.py
+++ instagramcopy/newslent/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from .models import News, PostAttachment
+from .forms import PostForm
 # Create your views here.
+def newslist(request):
+    news = News.objects.all().order_by('-publication_date')
+    return render(request, 'newslent/post_list.html', {'posts': news})
+
+def postdetails(request, pid):
+    news = News.objects.get(pk=pid)
+    images = PostAttachment.objects.filter(post_id = pid)
+    return render(request, 'newslent/details_page.html', {'post': news, 'images': images})
+
+def addPost(request):
+    if request.method != 'POST':
+        form = PostForm()
+    else:
+        form = PostForm(request.POST)
+        att = request.FILES.getlist('images')
+        if form.is_valid():
+            post = form.save(commit=False)
+            # add author to post
+            post.save()
+            for img in att:
+                PostAttachment.objects.create(post_id = post.pk, image=img)
+        return redirect(to='details', pid=post.pk)
+    return render(request, 'newslent/newpost.html', {'form':form})
+
+def editPost(request, pid):
+    post = News.objects.get(pk=pid)
+    post_att = PostAttachment.objects.filter(post_id=pid)
+
+    if request.method != 'POST':
+        form = PostForm(instance=post)
+    else:
+        form = PostForm(request.POST, instance=post)
+        
+        if form.is_valid():
+            post = form.save(commit=False)
+            att = request.FILES.getlist('images')
+            for img in att:
+                PostAttachment.objects.create(post_id = post.pk, image=img)
+            chosen = request.POST.getlist('attachments')
+            for img_id in chosen:
+                PostAttachment.objects.get(pk=int(img_id)).delete()
+            post.edited = True
+            post.save()
+           
+        return redirect(to='details', pid=post.pk)
+    return render(request, 'newslent/editpost.html', {'form':form, "post_att":post_att})
+
+def deletePost(request, pid):
+    post = News.objects.get(pk=pid)
+    post.delete()
+    return redirect(to='news_list')
